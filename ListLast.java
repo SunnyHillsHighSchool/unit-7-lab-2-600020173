22e90e22143ce30a1cd7a78c2aec34961670f5e0
Jonathan Hsing <600020173@fjuhsd.org>
2021-02-04 04:05:50 +0000

    finished ListLast method

--- ListLast.java
+++ ListLast.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name -Jonathan Hsing
+//Date -2/3/21
 
 import java.util.List;
 import java.util.ArrayList;
@@ -10,6 +10,27 @@ public class ListLast
 {
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+	//Create a variable and set it equal to the last item of the array
+  	int a = ray.get(ray.size() - 1);
+    //Create a variable to track the current array value
+    int b = 0;
+    //Create a boolean = false
+    boolean c = false;
+    //Use a for loop to loop each but the last element of the array
+    for(int x = 0; x < (ray.size()-1); x ++)
+    {
+      //Set the current array value variable to the current value of the array
+      b = ray.get(x);
+      //If the current array value variable is equal to the last item of the array variable
+      if(a==b)
+      {
+        //Set the boolean true
+        c = true;
+        //End if statement
+      }
+      //End loop
+    }
+    //Return boolean
+    return c;
 	}
 }
